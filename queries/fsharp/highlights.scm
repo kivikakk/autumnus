929cf7382c78edf1a6027105f2801c04ff96f401
kivikakk <1915+kivikakk@users.noreply.github.com>
2025-06-22 10:36:42 +0000

    chore: update queries

--- queries/fsharp/highlights.scm
+++ queries/fsharp/highlights.scm
@@ -123,7 +123,7 @@
 ((argument_patterns
   (long_identifier
     (identifier) @character.special))
-  (#lua-match? @character.special "^\_.*"))
+  (#lua-match? @character.special "^_.*"))
 
 (member_defn
   (method_or_prop_defn
@@ -200,6 +200,7 @@
   "#line" @keyword.directive)
 
 (attribute
+  target: (identifier)? @keyword
   (_type) @attribute)
 
 [
